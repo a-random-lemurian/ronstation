f62121c41e9d2fe5d318b8bb00e6be71741511ef
Merrokitsune <xudcii9i@duck.com>
2025-01-03 03:07:06 +0700

    Readd Omega.

--- Content.IntegrationTests/Tests/PostMapInitTest.cs
+++ Content.IntegrationTests/Tests/PostMapInitTest.cs
@@ -66,7 +66,8 @@ namespace Content.IntegrationTests.Tests
             "Gax",
             "Rad",
             "Europa",
-            "Saltern-2" //Ronstation
+            "Saltern-2", //Ronstation
+            "Omega-Lul" //Ronstation
         };
 
         /// <summary>
